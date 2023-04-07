7e86169aa7b9fb46b11d101fa1da09024f5960eb
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2023-04-08 01:00:02 +0530

    backend auth start

---
+++ backend/soc-med-auth-service/src/main/java/com/dimuthu/socmedauthservice/SocMedAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dimuthu.socmedauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocMedAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocMedAuthServiceApplication.class, args);
+	}
+
+}
