0e44dc4cdd3cffe36aa2e19b373ce4bf5c6de1dc
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2023-04-13 23:53:58 +0530

    having both cra and vite app working

--- client/src/components/pages/ExploreFriendsPage.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react';
-import UserList from '../components/users/UserList';
-
-const ExploreFriendsPage: FC = () => {
-  return <UserList />;
-}
-
-export default ExploreFriendsPage;
