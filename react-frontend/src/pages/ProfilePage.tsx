2ef75ca10043f29e4c5bba1c11ea27d4ce912630
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2023-04-15 12:52:02 +0530

    dummy material on profile page

--- react-frontend/src/pages/ProfilePage.tsx
+++ react-frontend/src/pages/ProfilePage.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from 'react';
-import { Col } from 'antd';
+import { Col, Row } from 'antd';
 
 const ProfilePage: FC = () => {
     return (
-      <Col span={16} offset={4} style={{ marginTop: '4rem' }}>
-        <h1>Hello there</h1>
+      <Col span={16} offset={4} style={{ marginTop: '4rem', height: '100%' }}>
+        <Row style={{height: '25%', backgroundColor: 'cyan'}}>
+          Hello there
+        </Row>
       </Col>
     );
 };
